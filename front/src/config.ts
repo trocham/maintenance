b5112a53234a8c18e507ad2c2a70f1b25d5316c2
trocham <rochateofilo@gmail.com>
2021-01-28 12:03:39 -0600

    ordering data

--- front/src/config.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export default{
-    MONGO_DB: process.env.MONGO_DB || 'maintenance-db',
-    MONGO_USR: process.env.MONGO_USR || 'trocha',
-    MONGO_PWR: process.env.MONGO_PWR || 'trochadb1',
-    APP_PORT:process.env.APP_PORT || 3001,
-}
--- front/src/index.ts
+++
@@ -1,6 +0,0 @@
-import app from "./app";
-import "./database"
-
-app.listen (app.get('port'),()=>{
-    console.log("Starting server... init on port" , app.get('port'));
-})
